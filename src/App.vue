6aafed06dfc841767e1ee746ea7ffd03277301d1
Ghosterbeef <Ghosterbeef@github.com>
2021-09-05 20:40:40 +0300

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,52 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <SideBarNav>
+
+  </SideBarNav>
   <router-view/>
 </template>
 
+<script>
+import SideBarNav from "./components/SideBarNav/SideBarNav";
+
+export default {
+  components: {SideBarNav}
+}
+</script>
+
 <style lang="scss">
+*, *::before, *::after {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+html,body{
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  display: flex;
+  background-color: gold;
+  height: 100%;
+  width: 100%;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+//#nav {
+//  padding: 30px;
+//
+//  a {
+//    font-weight: bold;
+//    color: #2c3e50;
+//
+//    &.router-link-exact-active {
+//      color: #42b983;
+//    }
+//  }
+//}
 </style>
+
---
+++ src/components/SideBarNav/SideBarNav.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav>
+    <div class="header">
+      <slot name="header">
+
+      </slot>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "SideBarNav"
+}
+</script>
+
+<style scoped lang="scss">
+nav {
+    display: flex;
+    flex-direction: column;
+    width: 300px;
+    height: 100%;
+    background-color: $bg-sidebar;
+}
+</style>
