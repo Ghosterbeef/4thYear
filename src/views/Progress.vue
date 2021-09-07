7fc764d0e433669cd56c53c870e99f0a31c78fd0
Ghosterbeef <Ghosterbeef@github.com>
2021-09-08 00:03:23 +0300

    0.0.19 Release Progress page now based on firebase information

--- src/views/Progress.vue
+++ src/views/Progress.vue
@@ -2,149 +2,31 @@
   <div class="progress">
     <section class="version">
       <Title :level="5">В разработке</Title>
-      <List>
-        <li>
+      <List v-if="progressData.inProgress">
+        <li v-for="(task, index) in progressData.inProgress.tasks" :key="index">
           <span class="name">
-            <Title :level="6">Лабораторная работа №1 </Title>
-            <Emphasis styled-like="danger" lang="en" is-bold is-underline>Epic</Emphasis>
+            <Title :level="6">{{task.name}}</Title>
+            <Emphasis styled-like="danger" lang="en" is-bold is-underline v-if="task.isEpic">Epic</Emphasis>
           </span>
           <Paragraph styled-like="description">
-            В разработке находится раздел содержащий выполнение лабораторной работы №1 по ПРУН
-          </Paragraph>
-        </li>
-        <li>
-          <span class="name">
-            <Title :level="6">Редизайн</Title>
-          </span>
-          <Paragraph styled-like="description">
-            Многие элементы требуют доработки или полной переработки дизайна.
-            Меню и страница SOON первые в очереди на переработку
-          </Paragraph>
-        </li>
-        <li>
-          <span class="name">
-            <Title :level="6">Адаптив</Title>
-          </span>
-          <Paragraph styled-like="description">
-            Доработка верстки для улучшения UX
-          </Paragraph>
-        </li>
-        <li>
-          <span class="name">
-            <Title :level="6">Прогресс </Title>
-            <Emphasis styled-like="danger" lang="en" is-bold is-underline>Epic</Emphasis>
-          </span>
-          <Paragraph styled-like="description">
-            Планируется перенсти отрисовку прогресса на циклы.
-            Хранение информации будет происходить либо в json на хостинге, либо в firebase
+            {{task.description}}
           </Paragraph>
         </li>
       </List>
     </section>
-    <section class="version">
-      <Title :level="5">0.0.18</Title>
+    <section class="version" v-for="(version) in progressData.versions.reverse()" :key="version.value">
+      <Title :level="5">{{version.value}}</Title>
       <Title :level="5">Реализовано</Title>
       <List>
-        <li>
+        <li v-for="(task, index) in version.tasks" :key="index">
           <span class="name">
-            <Title :level="6">Переменные</Title>
+            <Title :level="6">{{task.name}}</Title>
           </span>
-          <Paragraph styled-like="description">
-            90% стилей перенесны на переменные.
+          <div v-if="task.descriptions">
+          <Paragraph  v-for="(description, index) in task.descriptions" :key="index" :styled-like="description.type">
+            {{description.text}}
           </Paragraph>
-          <Paragraph styled-like="warning">
-            Оставшиеся 10% будут перенесены по мере необходимости
-          </Paragraph>
-        </li>
-        <li>
-          <span class="name">
-            <Title :level="6">Адаптив</Title>
-          </span>
-          <Paragraph styled-like="description">
-            Адаптив был частично переработан для улучшения использования полезного пространства на экране мобильных устройств.
-            Работа продолжается
-          </Paragraph>
-        </li>
-        <li>
-          <span class="name">
-            <Title :level="6">Компоненты типографии</Title>
-          </span>
-          <Paragraph styled-like="description">
-            Были добавлены компонеты: Акцент (выделение фрагмента текста), Автограф (компонет, для указания источника)
-          </Paragraph>
-        </li>
-        <li>
-          <span class="name">
-            <Title :level="6">Иконки</Title>
-          </span>
-          <Paragraph styled-like="description">
-            Был изменне подход для отрисовки иконок. Создан компонет для иконок, создана анимированная иконка для меню.
-          </Paragraph>
-          <Paragraph styled-like="warning">Иконки не окончательные</Paragraph>
-        </li>
-      </List>
-    </section>
-    <section class="version">
-      <Title :level="5">0.0.17</Title>
-      <Title :level="5">Реализовано</Title>
-      <List>
-        <li>
-          <span class="name">
-            <Title :level="6">Выпадающее меню</Title>
-          </span>
-          <Paragraph styled-like="description">
-            Элементы меню,
-            содержащие подпункты,
-            теперь являются выпадающими списками.
-          </Paragraph>
-        </li>
-        <li>
-          <span class="name">
-            <Title :level="6">Стили ссылок</Title>
-          </span>
-          <Paragraph styled-like="description">
-            Активные ссылки были переработаны.
-            Было добавлено затемнение цвета.
-          </Paragraph>
-        </li>
-        <li>
-          <span class="name">
-            <Title :level="6">Были добавлены типы параграфов</Title>
-          </span>
-          <Paragraph>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, nisi.
-          </Paragraph>
-          <Paragraph styled-like="description">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, nisi.
-          </Paragraph>
-          <Paragraph styled-like="success">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, nisi.
-          </Paragraph>
-          <Paragraph styled-like="warning">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, nisi.
-          </Paragraph>
-          <Paragraph styled-like="danger">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, nisi.
-          </Paragraph>
-        </li>
-      </List>
-    </section>
-    <section class="version">
-      <Title :level="5">0.0.15</Title>
-      <Title :level="5">Реализовано</Title>
-      <List>
-        <li>
-          <span class="name">
-            <Title :level="6">Страница прогресса</Title>
-          </span>
-        </li>
-        <li>
-          <span class="name">
-            <Title :level="6">Боковое меню</Title>
-          </span>
-          <Paragraph styled-like="description">
-            Функционал требует доработки
-          </Paragraph>
+          </div>
         </li>
       </List>
     </section>
@@ -159,7 +41,17 @@ import Emphasis from "@/components/Typography/Emphasis";
 
 export default {
   name: "Progress",
-  components: {Emphasis, List, Paragraph, Title}
+  components: {Emphasis, List, Paragraph, Title},
+  data() {
+    return {
+      progressData: {}
+    }
+  },
+  created() {
+    fetch('https://thyear-3e949-default-rtdb.europe-west1.firebasedatabase.app/progress.json')
+        .then(res => res.json())
+        .then(json => {this.progressData = json})
+  }
 }
 </script>
 
