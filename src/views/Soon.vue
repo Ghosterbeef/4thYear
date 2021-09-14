40adfc02ecb13e3e50339d30e83739f5141c5c9a
Ghosterbeef <Ghosterbeef@github.com>
2021-09-14 21:18:18 +0300

    0.1.22 Release Added new Chart on Soon page

--- src/views/Soon.vue
+++ src/views/Soon.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="soon">
-    <Title :level="2">Приложение находится в разработке</Title>
-    <Title :level="3">Следите за обновлениями</Title>
+    <Title :level="3">Приложение находится в разработке!</Title>
+    <Title :level="4">Следите за обновлениями.</Title>
     <Paragraph styled-like="description">Вы можете посмотреть списки готового функционала и тех, что находятся в
       разработке на соответствующей странице -
       <Link to="/progress">
         <Icon icon="progress" styled-like="link"/>
-      </Link>
+      </Link>.
     </Paragraph>
-    <Title :level="4">Я не сижу без дела =) Поверьте этому графику!</Title>
-    <LineChartWrapper :data="transformData(commits)"/>
+    <Title :level="5">Я не сижу без дела =) Поверьте этим графикам:</Title>
+    <Title :level="6">График количества комитов в день.</Title>
+    <LineChartWrapper :data="transformCommits"/>
+    <Title :level="6">График количества добавлений и удалений контента в неделю.</Title>
+    <LineChartWrapper :data="transformCode" :options="codeOptions"/>
   </div>
 </template>
 
@@ -26,16 +29,14 @@ export default {
   data() {
     return {
       commits: [],
-      chartOptions: {
-        responsive: true,
-        maintainAspectRatio: false,
+      codeAdd: [],
+      codeDelete: [],
+      codeOptions: {
         scales: {
           y: {
-            beginAtZero: true,
-            grace: '5%',
             title: {
               display: true,
-              text: 'Коммиты'
+              text: 'Количество строк'
             }
           },
           x: {
@@ -45,16 +46,13 @@ export default {
             }
           }
         },
-        plugins: {
-          legend: {
-            display: false
-          }
-        }
       }
     }
   },
   async created() {
     const github = await fetch('https://api.github.com/repos/Ghosterbeef/4thYear/stats/commit_activity').then(res => res.json())
+    const github1 = await fetch('https://api.github.com/repos/Ghosterbeef/4thYear/stats/code_frequency').then(res => res.json())
+    console.log(github1)
     const firstWeek = github.findIndex(i => i.total !== 0)
     github.splice(0, firstWeek)
     const date = new Date(github[0].week * 1000)
@@ -67,10 +65,22 @@ export default {
           }
       }))
     })
+    github1.forEach((item) => {
+      const startDate = new Date(item[0] * 1000)
+      const endDate = new Date((item[0] * 1000) + 7 * 24 * 60 * 60 * 1000)
+      this.codeAdd.push({
+        code: item[1],
+        date: `${startDate.getDate()}/${startDate.getMonth() + 1}-${endDate.getDate()}/${endDate.getMonth() + 1}`
+      })
+      this.codeDelete.push({
+        code: item[2],
+        date: `${startDate.getDate()}/${startDate.getMonth() + 1}-${endDate.getDate()}/${endDate.getMonth() + 1}`
+      })
+    })
   },
-  methods: {
-    transformData: function (array) {
-      const temp = array.filter(item => item !== undefined)
+  computed: {
+    transformCommits: function () {
+      const temp = this.commits.filter(item => item !== undefined)
       return {
         labels: [...temp.map(element => element.date)],
         datasets: [{
@@ -83,6 +93,27 @@ export default {
         },]
       }
     },
+    transformCode: function () {
+      return {
+        labels: [...this.codeAdd.map(element => element.date)],
+        datasets: [
+          {
+            label: "Добавления",
+            data: this.codeAdd.map(element => element.code),
+            borderColor: ['rgb(160,239,201)'],
+            pointBackgroundColor: ['#27b062'],
+            tension: 0.2
+          },
+          {
+            label: "Удаления",
+            data: this.codeDelete.map(element => Math.abs(element.code)),
+            borderColor: ['rgb(255,181,177)'],
+            pointBackgroundColor: ['crimson'],
+            tension: 0.2
+          }
+        ]
+      }
+    }
   }
 }
 </script>
